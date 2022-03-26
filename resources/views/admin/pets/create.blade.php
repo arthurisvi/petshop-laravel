2a26080d15bc3e2b51a8e9017eb8a30531f0b5ac
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-03-26 14:36:45 -0300

    feat: implmenting applications views

---
+++ resources/views/admin/pets/create.blade.php
@@ -0,0 +1,42 @@
+<div>
+    <h1>Cadastrar Pet</h1>
+    <div>
+        <h2>Pet</h2>
+
+        @if($errors->any())
+            <div>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action=" {{ route('pets.create') }}" method="post">
+            @csrf
+            <input type="text" name="name" id="name" placeholder = "Nome" value="{{old('name')}}">
+            <input type="number" name="age" id="age" placeholder = "Idade" value="{{old('age')}}">
+            <select name = "type" id = "type">
+                <option selected disabled>Tipo</option>
+                <option value = "Gato">Gato</option>
+                <option value = "Cachorro">Cachorro</option>
+            </select>
+            <input type="text" name="breed" id="breed" placeholder = "Raça" value="{{old('breed')}}">
+
+
+            <select name = "ownerId" name = "ownerId">
+                <option selected disabled>Dono</option>
+                @foreach ($owners as $owner)
+                <option value = {{ $owner->id }}>{{ $owner->name }} - Tel: {{ $owner->tel }}</option>
+                @endforeach
+            </select>
+
+            <button type ="submit">Cadastrar</button>
+        </form>
+    </div>
+</div>
+
+
+
+
---
+++ resources/views/admin/pets/index.blade.php
@@ -0,0 +1,19 @@
+<h1>Teste</h1>
+
+<a href="{{ route('pets.viewCreate') }}">Cadastrar novo Pet</a>
+
+@if(session()('messageDelete'))
+    <div>
+        {{ session('messageDelete') }}
+    </div>
+@endif
+
+@foreach ($pets as $pet)
+    <p>{{ $pet->name }}</p>
+
+    <form action="{{ route('pets.delete', $pet->id)}}" method = "post">
+    @csrf
+    @method("delete")
+    <button type = "submit">Excluir</button>
+    </form>
+@endforeach
