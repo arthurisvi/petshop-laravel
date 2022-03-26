63cf635b39c7d1dff6de9e8d12b0d338d36525fb
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-03-26 05:16:16 -0300

    feat: creating migrations pet and owner relationship

---
+++ database/migrations/2022_03_26_044025_create_pets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('idade');
+            $table->string('type');
+            $table->string('breed');
+            $table->unsignedBigInteger('id_owner')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('id_owner')->references('id')->on('owners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
